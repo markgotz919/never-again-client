980e0dbce02c6d8c3e5c1a37803b21e58875eced
markgotz919 <mg108@cox.net>
2018-08-22 13:31:45 -0400

    worked on header, and signin-out, change pw, and signup now work

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const store = require('../store')
+
+// const mealEvents = require('../items/events-meals.js')
+
+
+const signUpSuccess = function (data) {
+  $('#message').text('Signed up successfully')
+  $('#message').css('background-color', 'green')
+  
+}
+
+const signUpFailure = function (error) {
+  $('#message').text('Error on sign up')
+  $('#message').css('background-color', 'red')
+  
+}
+
+const signInSuccess = function (data) {
+  $('#message').text('Signed in successfully')
+  $('#message').css('background-color', 'green')
+ 
+  store.user = data.user
+  $("#change-password, #sign-out, #new-meal,#show-meals, #delete-meal, #update-meal").show();
+  $("#sign-up, #sign-in").hide();
+  
+
+}
+
+const signInFailure = function (error) {
+  $('#message').text('Error on sign in')
+  $('#message').css('background-color', 'red')
+  
+}
+
+const signOutSuccess = function () {
+  $('#message').text('Signed out successfully')
+  $('#message').css('background-color', 'green')
+
+  store.user = null
+  $("#change-password, #sign-out, #new-meal,#show-meals,#delete-meal, #meals,#update-meal").hide();
+  $("#sign-up, #sign-in").show();
+}
+
+const signOutFailure = function (error) {
+  $('#message').text('Error on sign out')
+  $('#message').css('background-color', 'red')
+  // console.error('signOutFailure ran. Error is :', error)
+}
+
+const changePasswordSuccess = function () {
+  $('#message').text('Changed password successfully')
+  $('#message').css('background-color', 'green')
+  // console.log('changePasswordSuccess ran and nothing was returned!')
+}
+
+const changePasswordFailure = function (error) {
+  $('#message').text('Error on change password')
+  $('#message').css('background-color', 'red')
+  // console.error('changePasswordFailure ran. Error is :', error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  changePasswordSuccess,
+  changePasswordFailure
+}
