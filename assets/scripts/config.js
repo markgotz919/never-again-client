628b63331c1419ce68f2d3fe538acf64e5889805
markgotz919 <mg108@cox.net>
2018-08-24 08:56:44 -0400

    entered the url for remote

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://shielded-atoll-43263.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
